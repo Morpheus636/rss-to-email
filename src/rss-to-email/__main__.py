43255282d54e8277758af2966537d46efd4c01ae
Josh Levin <morpheus636@morpheus636.com>
2022-10-30 17:42:10 -0400

    moved sleep to end

--- src/rss-to-email/__main__.py
+++ src/rss-to-email/__main__.py
@@ -6,9 +6,10 @@ import rss
 
 
 while True:
-    # Wait 10 mins in between polls
-    time.sleep(600)
     # Get the list of new entries and send each of them as an email.
     new_entries = rss.get_new_entries(config.load_feeds())
     for entry in new_entries:
         email_api.send_entry(entry)
+
+    # Wait 10m between polls
+    time.sleep(600)
