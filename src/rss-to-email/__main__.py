90306821236871bb678521af701cdcc403ba7c8d
Josh Levin <morpheus636@morpheus636.com>
2022-10-30 17:25:39 -0400

    runs infinitely

--- src/rss-to-email/__main__.py
+++ src/rss-to-email/__main__.py
@@ -1,10 +1,14 @@
+import time
+
 import config
 import email_api
 import rss
 
 
-feeds = config.load_feeds()
-new_entries = rss.get_new_entries(feeds)
-print(new_entries)
-for entry in new_entries:
-    email_api.send_entry(entry)
+while True:
+    # Wait 10 mins in between polls
+    time.sleep(600)
+    # Get the list of new entries and send each of them as an email.
+    new_entries = rss.get_new_entries(config.load_feeds())
+    for entry in new_entries:
+        email_api.send_entry(entry)
