f7e7db5aa8eef2d72eb5699f3697c5d594325724
Josh Levin <morpheus636@morpheus636.com>
2022-11-11 05:30:02 -0500

    allowed null entries in the last parsed feed

--- src/rss-to-email/rss.py
+++ src/rss-to-email/rss.py
@@ -2,25 +2,34 @@ import config
 import feedparser
 
 
-def get_new_entries(feeds: dict) -> list:
+def get_new_entries(feeds: dict, count: int = 0) -> list:
     """Gets a list of entries added to each feed since the last time it was parsed.
 
     :param feeds: Dict of feed as loaded py `config.load_feeds()`
     :type feeds: dict
+    :param count: Optional - tells the parser how many entries to return from
+    the top of the list. If specified, the parser will ignore the last_entry field.
+    :type count: int
     :return: List of tuples for each new entry in the format (feed_name, entry)
     :rtype: list
     """
     new_entries = []
     for feed in feeds:
         parsed_feed = feedparser.parse(feeds[feed]["url"])
-        # Go through entries and process any that were published since the feed was last parsed.
-        for entry in parsed_feed.entries:
-            # If the entry is new, add it to the list to return.
-            if entry.link != feeds[feed]["last_entry"]:
-                new_entries.append((feed, entry))
-            # Break if the entry is not new.
-            else:
-                break
+        # If the count was specified, add the first x-number of entries to the list to return as tuples.
+        if count:
+            for i in range(0, count):
+                new_entries.append((feed, parsed_feed.entries[i]))
+
+        # If the count was not specified, return each new entry until the feed was last parsed.
+        else:
+            for entry in parsed_feed.entries:
+                # If the entry is new, add it to the list to return.
+                if entry.link != feeds[feed]["last_entry"]:
+                    new_entries.append((feed, entry))
+                # Break if the entry is not new.
+                else:
+                    break
 
         # Update the last entry to be the most recent item.
         feeds[feed]["last_entry"] = parsed_feed.entries[0].link
